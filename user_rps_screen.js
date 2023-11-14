693b166dcb6f09b8c4feb09da74ddfee83c78f72
zerome <iirony@naver.com>
2023-11-14 23:35:23 +0900

    feat : 통신 부분 구현

--- user_rps_screen.js
+++ user_rps_screen.js
@@ -1,3 +1,9 @@
+const SDK = window.AFREECA.ext;
+const extensionSdk = SDK();
+const TIMER = 'timer';
+const SELECTCARD = 'selectCard';
+const END = 'end';
+
 const selectbtn = document.querySelector('#selectbtn');
 const gamecardClasses = document.querySelectorAll('.gamecard');
 const resultmsg = document.querySelector('#resultmsg');
@@ -11,6 +17,34 @@ const paperCard = document.querySelector('#paperCard');
 const defaultClasses = document.querySelectorAll('.default');
 const waitClasses = document.querySelectorAll('.wait');
 const completeClasses = document.querySelectorAll('.complete');
+
+let userSelectCard = ''; // 유저가 선택한 카드
+let isGameStart = false;
+const elapsedImg = ['img1', 'img2', 'img3', 'img4', 'img5'];
+
+const handleBroadcastReceived = (action, message, fromId) => {
+    // 타이머 액션
+    if(action === TIMER) {
+        // 초기에 한번만
+        if(!isGameStart) {
+            setWaitDisplay('show');
+            setDefaultDisplay('hide');
+            isGameStart = true;
+        }
+
+        if(elapsedImg.length > 0) {
+            console.log(elapsedImg.pop());    // 카운트 이미지 출력
+        }
+    }
+    // 게임 종료 액션
+    else if(action === END) {
+        showResult(message);
+    }
+
+    console.log(action, message, fromId);
+}
+
+extensionSdk.broadcast.listen(handleBroadcastReceived);
 
 const setDefaultDisplay = (action) => {
     defaultClasses.forEach(function (el) {
@@ -33,62 +67,58 @@ const setCompleteDisplay = (action) => {
     });
 }
 
-const showResult = () => {
+const getGameResultForUser = (bjSelectCard) => {
+    let gameResult = 'draw';
+
+    // 유저가 게임에 참여했다는 전제
+    if (userSelectCard && (userSelectCard !== bjSelectCard)) {
+        if (userSelectCard === 'scissors') {
+            gameResult = 'lose';
+            if (bjSelectCard === 'paper') {
+                gameResult = 'win';
+            }
+        } else if (userSelectCard === 'rock') {
+            gameResult = 'lose';
+            if (bjSelectCard === 'scissors') {
+                gameResult = 'win';
+            }
+        } else if (userSelectCard === 'paper') {
+            gameResult = 'lose';
+            if (bjSelectCard === 'rock') {
+                gameResult = 'win';
+            }
+        }
+    }
+
+    return gameResult;
+}
+
+const showResult = (oResult) => {
     setWaitDisplay('hide');
     setCompleteDisplay('show');
 
-    // TODO. 유저가 고른 카드로 보여주도록 변경
-    const cardSelected = document.querySelector('.cardselected');
     gamecardClasses.forEach(function (el) {
-        if (!el.classList.contains('cardselected')) {
+        // BJ가 고른 카드만 보여주기
+        if (el.dataset.card !== bjcard) {
             el.style.display = 'none';
         }
     });
 
-    let gameResult = '';
-    const usrCardId = cardSelected.id;
-    const bjCardId = 'rockCard';    // TODO. 유저가 선택한 카드 아이디 받아와야 함
-    if (usrCardId === bjCardId) {
-        gameResult = 'draw';
-    } else {
-        if (usrCardId === 'scissorsCard') {
-            gameResult = 'lose';
-            if (bjCardId === 'paperCard') {
-                gameResult = 'win';
-            }
-        } else if (usrCardId === 'rockCard') {
-            gameResult = 'lose';
-            if (bjCardId === 'scissorsCard') {
-                gameResult = 'win';
-            }
-        } else if (usrCardId === 'paperCard') {
-            gameResult = 'lose';
-            if (bjCardId === 'rockCard') {
-                gameResult = 'win';
-            }
-        }
-    }
-    console.log('User Game Result: ' + gameResult);    // TODO. 게임 결과 이미지로 대체
+    const gameResult = getGameResultForUser(oResult.bjcard);
+    console.log('나의 승부 결과', gameResult) // TODO 상단 이미지
 
-    msg = 'BJ가 모두를 이겼습니다!';
-    const allCnt = 100;
-    const winNicks = ['정보경','ababaabababa','손가을손가을','폭주중','정보경','이나영','손가을','폭주중','정보경','이나영','손가을','폭주중','정보경','이나영','손가을','폭주중','정보경','이나영','손가을','폭주중'];
-    const winCnt = winNicks.length;
+    // 공통 메세지
+    resultmsg.innerText = oResult.msg;
 
-    if (winCnt > 0) {
-        msg = `${allCnt}명중에 ${winCnt}명이 BJ를 이겼습니다!`;
-        idarea.value = '<< 이긴 유저 >>\n' + winNicks.join('\n');
+    if(oResult.winners) {
+        idarea.value = oResult.winners;
         idarea.style.display = '';
     }
-
-    resultmsg.innerText = msg;
 }
 
 // 게임카드 click
 gamecardClasses.forEach((target) => target.addEventListener("click", function (e) {
     const cardselected = document.querySelector('.cardselected');
-    const cardData = target.dataset.card;
-    console.log(cardData);
 
     // 기존 선택된거 지우기
     if (cardselected) {
@@ -98,27 +128,27 @@ gamecardClasses.forEach((target) => target.addEventListener("click", function (e
     target.classList.add('cardselected');
 }));
 
-// TODO. default상태에서 선택하기 누르면 게임 화면으로 이동하는데, 유저 이벤트 받아서 카운트 화면으로 넘어가도록 변경 필요
 // 선택하기 click
 selectbtn.addEventListener('click', function () {
-    setWaitDisplay('show');
-    setDefaultDisplay('hide');
+    const cardselected = document.querySelector('.cardselected');
 
-    let elapsedTime = 0;
-    let elapsedIdx = 0;
-    let elapsedImg = ['img1', 'img2', 'img3', 'img4', 'img5'];
-    const setIntervaltimer = setInterval(function () {
-        elapsedTime += 1000; // 1초씩 증가
-        console.log(elapsedImg[elapsedIdx]);    // 카운트 이미지 출력
-        elapsedIdx++;
+    // 선택한 카드가 없을 때
+    if(!cardselected) {
+        const toastElList = document.querySelectorAll('.toast');
+        const toastBody = document.querySelector('.toast-body');
+        const toastList = [...toastElList].map(toastEl => new bootstrap.Toast(toastEl));
 
-        console.log()
-        // 타이머 종료
-        if (elapsedTime >= 5000) {
-            console.log('end')
-            clearInterval(setIntervaltimer);
-            showResult();
-            return;
-        }
-    }, 1000); // 1초마다 실행
+        toastList.forEach(toast => {
+            toastBody.innerText = '카드를 선택해야 합니다!';
+            toast.show();
+        }) 
+
+        return; // 종료
+    }
+
+    userSelectCard = cardselected.dataset.card;
+    console.log('select',userSelectCard);
+
+    // BJ에게 선택한 카드 전송
+    extensionSdk.broadcast.send(SELECTCARD, userSelectCard);
 });
