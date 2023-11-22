fed732bb50cd15fb0d5f4f8d1e569d5611aad42d
zerome <iirony@naver.com>
2023-11-23 01:58:36 +0900

    feat : 전체 UI 붙이기, 이벤트 추가

--- bj_user5wh1_screen.js
+++ bj_user5wh1_screen.js
@@ -7,6 +7,14 @@ const nextbtn = document.querySelector('#nextbtn');
 const homebtn = document.querySelector('#homebtn');
 const chatClasses = document.querySelectorAll('.chat');
 
+let userSetting = {
+    type: '', star: 0
+};
+let inputArr = {
+    when: [], where: [], who: [], how: [], what: []
+};
+const nextTitle = {when: '2. 어디서 ?', where: '3. 누가 ?', who: '4. 어떻게 ?', how: '5. 무엇을 ?'};
+
 extensionSdk.broadcast.send(START_GAME, 'main');
 
 const setDefaultDisplay = (action) => {
@@ -25,20 +33,91 @@ const setProcessDisplay = (action) => {
     });
 }
 
+const setLastDisplay = (action) => {
+    const lastClasses = document.querySelectorAll('.last');
+
+    lastClasses.forEach(function(el) {
+        el.style.display = action === 'show' ? '' : 'none';
+    });
+}
+
+const showProcessView = () => {
+    setDefaultDisplay('hide');
+    setProcessDisplay('show');
+    setLastDisplay('hide');
+
+    const body = document.querySelector('body');
+    body.style.backgroundColor = '#e9a329';
+    body.style.color = 'black';
+}
+
+const showLastView = () => {
+    setDefaultDisplay('hide');
+    setProcessDisplay('hide');
+    setLastDisplay('show');
+
+    const body = document.querySelector('body');
+    body.style.backgroundColor = '#eb3251';
+    body.style.color = 'white';
+    nextbtn.style.display = 'none';
+    document.querySelector('#backbtn').style.display = 'block';
+}
+
+const setUserSetting = () => {
+    const userList = document.getElementsByName('user');
+            
+    userList.forEach((node) => {
+        if(node.checked)  {
+            userSetting.type = node.value;
+            userSetting.star = document.querySelector('#starcnt').value;
+            return;
+        }
+    }) 
+}
+
+const pushChatOn = (view) => {
+    const chatOn = document.querySelectorAll('.chat.on');
+
+    chatOn.forEach((node) => {
+        inputArr[view].push(node.innerText);
+    })
+
+    console.log(userSetting)
+    console.log(inputArr)
+}
+
+const changeTitle = (view) => {
+    document.querySelector('#w_title').innerText = nextTitle[view];
+}
+
+// Next 버튼
 nextbtn.addEventListener('click', function() {
     const view = ['guide', 'when', 'where', 'who', 'how', 'what', 'last'];
+    console.log(view[viewIdx]);
 
-    switch (view[viewIdx+1]) {
+    switch (view[viewIdx]) {
+        case 'guide':
+            showProcessView();
+            setUserSetting();
+            break;
         case 'when':
-            setDefaultDisplay('hide');
-            setProcessDisplay('show');
-            const body = document.querySelector('body');
-            body.style.backgroundColor = '#e9a329';
-            body.style.color = 'black';
+        case 'where':
+        case 'who':
+            changeTitle(view[viewIdx]);
+            pushChatOn(view[viewIdx]);
+            break;
+        case 'how':
+            changeTitle(view[viewIdx]);
+            pushChatOn(view[viewIdx]);
+            nextbtn.innerText = '완료!';
+            break;
+        case 'what':
+            pushChatOn(view[viewIdx]);
+            showLastView();
+            break;
+        case 'last':
             break;
     }
-
-    // showProcessView();
 
     viewIdx += 1;
 });
@@ -46,6 +125,11 @@ nextbtn.addEventListener('click', function() {
 // 홈버튼 click
 homebtn.addEventListener('click', function() {
     window.location = './bj_screen.html';
+});
+
+// 뒤로가기 click
+document.querySelector('#backbtn').addEventListener('click', function() {
+    window.location = './bj_user5wh1_screen.html'
 });
 
 // 채팅버튼 click
@@ -59,4 +143,5 @@ chatClasses.forEach((target) => target.addEventListener("click", function(e){
         target.classList.add('on');
     }
 }));
+
 // 50자까지 가능
