c6fa09bdfbb2186bccb93baeb2ef2f1a35f98380
bo0216 <bo216@naver.com>
2023-11-30 14:40:41 +0900

    fix: 가위바위보 user가 패를 선택할 때 마다 bj에게 게임결과값 이벤트 보내기로 수정

--- user_rps_screen.js
+++ user_rps_screen.js
@@ -34,7 +34,7 @@ const setWaitDisplay = (action) => {
         el.style.display = action === 'show' ? '' : 'none';
     });
 
-    gamecardClasses.forEach(function(el) {
+    gamecardClasses.forEach(function (el) {
         el.style.pointerEvents = action === 'show' ? 'auto' : 'none';
     })
 }
@@ -57,13 +57,12 @@ const showHeaderResult = () => {
         // BJ가 고른 카드만 보여주기
         if (el.dataset.card == bjSelectCard) {
             el.classList.add('cardselected');
-        }
-        else {
+        } else {
             el.style.display = 'none';
         }
     });
 
-    if(gameResult) {
+    if (gameResult) {
         resultImg.src = IMG_DIRECTORY + gameResult + '.png';
     }
 }
@@ -75,7 +74,7 @@ const showUserList = (oResult) => {
     // 공통 메세지
     resultmsg.innerText = oResult.msg;
 
-    if(oResult.winners) {
+    if (oResult.winners) {
         idarea.value = oResult.winners;
         idarea.style.display = '';
     }
@@ -84,8 +83,12 @@ const showUserList = (oResult) => {
 const getGameResultForUser = () => {
     let gameResult = 'draw';
 
+    if (!userSelectCard) {
+        return gameResult;
+    }
+
     // 유저가 게임에 참여했다는 전제
-    if (userSelectCard && (userSelectCard !== bjSelectCard)) {
+    if (userSelectCard !== bjSelectCard) {
         if (userSelectCard === 'scissors') {
             gameResult = 'lose';
             if (bjSelectCard === 'paper') {
@@ -109,63 +112,62 @@ const getGameResultForUser = () => {
 
 // 게임카드 click
 gamecardClasses.forEach((target) => target.addEventListener("click", function (e) {
-    // 선택하기 완료한 경우
-    if(selectbtn.classList.contains('disabled')) {
-        return;
-    }
-
     const cardselected = document.querySelector('.cardselected');
-
     // 기존 선택된거 지우기
     if (cardselected) {
         cardselected.classList.remove('cardselected');
     }
 
     target.classList.add('cardselected');
-}));
 
-// 선택하기 click
-selectbtn.addEventListener('click', function () {
-    const cardselected = document.querySelector('.cardselected');
-
-    // 선택한 카드가 없을 때
-    if(!cardselected) {
-        const toastElList = document.querySelectorAll('.toast');
-        const toastBody = document.querySelector('.toast-body');
-        const toastList = [...toastElList].map(toastEl => new bootstrap.Toast(toastEl));
-
-        toastList.forEach(toast => {
-            toastBody.innerText = '카드를 선택해야 합니다!';
-            toast.show();
-        }) 
-
-        return; // 종료
-    }
-
-    userSelectCard = cardselected.dataset.card;
-
+    userSelectCard = target.getAttribute("data-card");
     gameResult = getGameResultForUser();
-    // console.log('나의 승부 결과', gameResult);
-
     // BJ에게 승부 결과 전송
     extensionSdk.broadcast.send(SELECTCARD_BY_USER, gameResult);
 
-    // 선택하기 비활성화
-    selectbtn.classList.add('disabled');
-});
+}));
+
+// 선택하기 click
+// selectbtn.addEventListener('click', function () {
+//     const cardselected = document.querySelector('.cardselected');
+//
+//     // 선택한 카드가 없을 때
+//     if (!cardselected) {
+//         const toastElList = document.querySelectorAll('.toast');
+//         const toastBody = document.querySelector('.toast-body');
+//         const toastList = [...toastElList].map(toastEl => new bootstrap.Toast(toastEl));
+//
+//         toastList.forEach(toast => {
+//             toastBody.innerText = '카드를 선택해야 합니다!';
+//             toast.show();
+//         })
+//
+//         return; // 종료
+//     }
+//
+//     userSelectCard = cardselected.dataset.card;
+//
+//     gameResult = getGameResultForUser();
+//     // console.log('나의 승부 결과', gameResult);
+//
+//     // BJ에게 승부 결과 전송
+//     extensionSdk.broadcast.send(SELECTCARD_BY_USER, gameResult);
+//
+//     // 선택하기 비활성화
+//     selectbtn.classList.add('disabled');
+// });
 
 const handleBroadcastReceived = (action, message, fromId) => {
     // 게임 시작 액션
-    if(action === START_GAME) {
-        if(message == 'main') {
+    if (action === START_GAME) {
+        if (message == 'main') {
             window.location = './user_screen.html';
-        }
-        else if(message == 'rps') {
+        } else if (message == 'rps') {
             window.location = './user_rps_screen.html';
         }
     }
     // 초기 타이머 액션
-    else if(action === TIMERSTART_BY_BJ) {
+    else if (action === TIMERSTART_BY_BJ) {
         setWaitDisplay('show');
         setDefaultDisplay('hide');
 
@@ -173,8 +175,8 @@ const handleBroadcastReceived = (action, message, fromId) => {
         bjSelectCard = message;
     }
     // 이후 타이머 액션
-    else if(action === TIMER_BY_BJ) {
-        if(message) {
+    else if (action === TIMER_BY_BJ) {
+        if (message) {
             cntImg.src = IMG_DIRECTORY + message + '.png';
         }
         else {
@@ -182,7 +184,8 @@ const handleBroadcastReceived = (action, message, fromId) => {
         }
     }
     // 게임 종료 액션
-    else if(action === END_BY_BJ) {
+    else if (action === END_BY_BJ) {
+
         showHeaderResult();
         showUserList(message);
     }
